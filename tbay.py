c1250746c7bcae8d691ed8047b6fc2bf2807f052
Tyler Hedegard <tchedegard@gmail.com>
2016-05-19 16:22:53 +0000

    initial commit

---
+++ tbay.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    start_time = Column(DateTime, default=datetime.utcnow)
+    
+Base.metadata.create_all(engine)
