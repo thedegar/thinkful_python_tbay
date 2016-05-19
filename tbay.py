398e7771476c154541c4798c4a5bdd3063ea79ef
Tyler Hedegard <tchedegard@gmail.com>
2016-05-19 16:38:38 +0000

    added users and bids tables

--- tbay.py
+++ tbay.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float
 
 class Item(Base):
     __tablename__ = "items"
@@ -20,4 +20,17 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    
+class Bid(Base):
+    __tablename__ = "bids"
+    
+    id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
+
 Base.metadata.create_all(engine)
