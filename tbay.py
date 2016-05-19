358649320c4db3080fa60c41edab44a5a2ea792b
Tyler Hedegard <tchedegard@gmail.com>
2016-05-19 19:04:12 +0000

    added db relationships

--- tbay.py
+++ tbay.py
@@ -2,15 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 from datetime import datetime
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 
-from sqlalchemy import Column, Integer, String, DateTime, Float
+# Relationships
+#Users should be able to auction multiple items - One(user) to Many(items)
+#Users should be able to bid on multiple items - One(user) to Many(bids)
+#Multiple users should be able to place a bid on an item - One(item) to One(bid)
 
 class Item(Base):
     __tablename__ = "items"
@@ -20,6 +24,9 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    bids = relationship("Bid",backref="items")
+    
 class User(Base):
     __tablename__ = "users"
     
@@ -27,10 +34,38 @@ class User(Base):
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
     
+    items = relationship("Item",uselist=False, backref="users")
+    bids = relationship("Bid",backref="users")
+    
 class Bid(Base):
     __tablename__ = "bids"
     
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
+    
+    user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'),nullable=False)
 
 Base.metadata.create_all(engine)
+
+#Add three users to the database
+tyler = User(username="tyler",password="thinkful")
+lila = User(username="lila",password="thinkful")
+kyle = User(username="kyle",password="thinkful")
+
+#Make one user auction a baseball
+baseball = Item(name="baseball")
+tyler.items = baseball
+
+#Have each other user place two bids on the baseball
+bid1 = Bid(price=10,users=lila,items=baseball)
+bid2 = Bid(price=20,users=kyle,items=baseball)
+bid3 = Bid(price=30,users=lila,items=baseball)
+bid4 = Bid(price=40,users=kyle,items=baseball)
+
+session.add_all([tyler, lila, kyle, bid1, bid2, bid3, bid4])
+session.commit()
+
+
+#Perform a query to find out which user placed the highest 
+
